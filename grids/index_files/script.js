105f71caf5ca7efd48dcb47aa6068b48a1a9309d
Ross <koepke@gmail.com>
2018-04-06 05:18:36 -0500

    - Navbar added, sticky. - Google Maps added - Bouncy icon on face-hover - Fixed City Silhouette.

--- grids/index_files/script.js
+++ grids/index_files/script.js
@@ -1,6 +1,7 @@
 /*
-     * Replace all SVG images with inline SVG
-     */
+* Replace all SVG images with inline SVG
+*/
+
 jQuery('img.article-img').each(function(){
   var $img = jQuery(this);
   var imgID = $img.attr('id');
@@ -30,3 +31,43 @@ jQuery('img.article-img').each(function(){
   }, 'xml');
 
 });
+
+/*
+* Runs the google map API
+*/
+
+var geocoder;
+var map;
+var markers = [];
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  list = [
+    [29.740582, -95.4515725],
+    [29.4947986, -95.09110620000001]
+  ];
+  var bounds = new google.maps.LatLngBounds();
+  list.forEach(function (data, index, array) {
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(list[index][0], list[index][1]),
+      map: map
+    });
+    markers.push(marker);
+
+    bounds.extend(marker.position);
+  });
+  map.fitBounds(bounds);
+
+}
+
+showme = function (index) {
+  if (markers[index].getAnimation() != google.maps.Animation.BOUNCE) {
+    markers[index].setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function(){ markers[index].setAnimation(null); }, 750);
+  } else {
+    markers[index].setAnimation(null);
+  }
+}
