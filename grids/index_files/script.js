1289f228ac728553140e85d0f7aca55bb6558e78
frances-chen <francesrchen@gmail.com>
2018-04-07 00:46:13 -0500

    Add additional portraits (.svg files flattened and minified), adjust navbar aesthetics, adjust portrait hover aesthetics, separate out map.html page

--- grids/index_files/script.js
+++ grids/index_files/script.js
@@ -21,7 +21,6 @@ jQuery('img.article-img').each(function(){
       $svg = $svg.attr('class', imgClass+' replaced-svg');
     }
 
-
     // Remove any invalid XML tags as per http://validator.w3.org
     $svg = $svg.removeAttr('xmlns:a');
 
@@ -63,11 +62,43 @@ function initMap() {
 
 }
 
-showme = function (index) {
+/* FRANCES NOTE: Rearranging placement of things on the website. As a result,
+changing map animation so there is a bounce upon clicking + someone's image
+pops up. */
+
+/* showme = function (index) {
   if (markers[index].getAnimation() != google.maps.Animation.BOUNCE) {
     markers[index].setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(function(){ markers[index].setAnimation(null); }, 750);
   } else {
     markers[index].setAnimation(null);
   }
-}
+} */
+
+/* 
+* Change tab color on navbar depending on the active page
+* Can't change :hover CSS because that's a pseudoselector and not an actual DOM element
+*/
+
+var activePage = $('.active').text();
+
+console.log(activePage);
+
+switch (activePage) {
+    case 'Home':
+      $('.active').css('background-color', 'rgba(57, 129, 243, 1)');
+      $('.active').css('color', 'white');
+      break;
+    case 'About Us':
+      $('.active').css('background-color', 'rgba(230, 39, 25, 1)');
+      $('.active').css('color', 'white');
+      break;
+    case 'Map':
+      $('.active').css('background-color', 'rgba(240, 181, 5, 1)');
+      $('.active').css('color', 'white');
+      break;
+    case 'Contact':
+      $('.active').css('background-color', 'rgba(50, 164, 81, 1)');
+      $('.active').css('color', 'white');
+      break;
+  };
