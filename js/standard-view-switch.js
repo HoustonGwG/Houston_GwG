b4b45f87494dae4f4a337ada68f4d6d85fa3261c
lourod1987 <louart87@gmail.com>
2018-04-11 20:27:31 -0500

    testing out standardized view creation

--- js/standard-view-switch.js
+++ js/standard-view-switch.js
@@ -1,3 +1,10 @@
 $(document).ready(() => {
+	$('section').removeClass();
+	$('h2').removeClass();
+	$('div').removeClass();
+	$('img').removeClass();
 	
+	$('body').addClass('BG');
+	$('p').addClass('para');
+	$('a').addClass('links');
 })
